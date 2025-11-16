ea387a9c37d950de8d936404eaa97e12ee6c8b0d
jakobsamuel2005-coder <jakob.samuel2005@gmail.com>
2025-11-16 16:09:05 +0200

    Create printerapp.py

---
+++ printerapp.py
@@ -0,0 +1,9 @@
+number1=2
+number2=3
+creator="Jakob Samuel"
+print("This app is made by: " + creator)
+print("The value of number1 is:",number1)
+print("The value of number2 is:",number2)
+print("The sum of number1 and number2 is:", number1 + number2)
+print("The difference of number1 and number2 is:", number1 - number2)       
+print("Number1 multiplied by number2 is:", number1 * number2)
